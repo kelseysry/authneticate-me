26a1200fbf7420e8e8e50fb94fa8534d0202852f
Kelsey Sry <ksry@alumni.stanford.edu>
2021-11-04 23:44:31 -0700

    profile dropdown menu

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -6,8 +6,9 @@ import * as sessionActions from '../../store/session';
 function ProfileButton({ user }) {
   const dispatch = useDispatch();
   const [showMenu, setShowMenu] = useState(false);
+  // false = menu is hidden
 
-  const openMenu = () => {
+  const openMenu = () => { // invoked by the onClick in the html button, opens menu
     if (showMenu) return;
     setShowMenu(true);
   };
@@ -16,11 +17,14 @@ function ProfileButton({ user }) {
     if (!showMenu) return;
 
     const closeMenu = () => {
-      setShowMenu(false);
+      setShowMenu(false); // trigger closing of dropdown menu
     };
 
+    // register event listener for click events on the entire page (document)
+    // so can close the dropdown menu if click anywhere on the page.
     document.addEventListener('click', closeMenu);
 
+    // cleanup function for the useEffect should remove this event listener
     return () => document.removeEventListener("click", closeMenu);
   }, [showMenu]);
 
@@ -32,7 +36,7 @@ function ProfileButton({ user }) {
   return (
     <>
       <button onClick={openMenu}>
-         <i class="far fa-user-circle"></i>
+         <i className="far fa-user-circle"></i>
         {/* <i className="fas fa-user-circle" /> */}
       </button>
       {showMenu && (
