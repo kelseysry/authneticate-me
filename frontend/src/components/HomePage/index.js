1b5fa08cb018bcb01aec812abe20cadfe9ce068a
Kelsey Sry <ksry@alumni.stanford.edu>
2021-11-09 11:50:30 -0800

    display solo pic on home page

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,8 +1,17 @@
+import pictures from '../../data/pictures'
 
+console.log("pictures", pictures)
 
 const HomePage = () => {
 
 
-  
+  return (
+    // <div>hi</div>
+    <img src ={pictures.collection[0].imageUrl} alt={"hiii"}/>
+    // <img src={image.baseimageurl} key={image.imageid} alt={art.title} />
+  )
+
 
 }
+
+export default HomePage
