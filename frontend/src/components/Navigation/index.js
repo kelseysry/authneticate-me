375dd5eda07889e14ea1da98f1418cab0e904dce
Kelsey Sry <ksry@alumni.stanford.edu>
2021-11-09 15:11:35 -0800

    favicon

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -6,7 +6,7 @@ import ProfileButton from './ProfileButton';
 import * as sessionActions from '../../store/session'
 import './Navigation.css';
 import { useDispatch } from "react-redux";
-
+import './Navigation.css'
 
 function Navigation({ isLoaded }){
   const dispatch = useDispatch();
@@ -39,7 +39,7 @@ function Navigation({ isLoaded }){
   return (
     <ul>
       <li>
-        <NavLink exact to="/">Home</NavLink>
+        <NavLink activeClassName="home" exact to="/">Home</NavLink>
         {isLoaded && sessionLinks}
       </li>
     </ul>
