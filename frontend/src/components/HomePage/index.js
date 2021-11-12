a40a8578454ac2a8e1893f46c4c87270edfbc3a9
Kelsey Sry <ksry@alumni.stanford.edu>
2021-11-12 13:51:30 -0800

    details

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -35,10 +35,10 @@ const HomePage = () => {
       <div className="about-container">
         <div className="about">
             <div className="about-header">
-              About Mab Mab
+              About
             </div>
             <div className="about-details">
-              More details
+            I worked for about a year in Phnom Penh, Cambodia and became prettly well acquainted with all the restaurants there since I ate out for dinner daily. Food in Cambodia is incredibly fresh and cheap! I always bring my camera with me when I eat out so I managed to take a snapshot of all the meals I had in Cambodia and added these photos as my seeder data. The name Mab Mab is play on words from the Cambodian word "cah mab." "Cah mab" means fat in the Khmer language. It's typical to have your elder nickname you "mab mab" in a cute and endearing way.
             </div>
         </div>
       </div>
