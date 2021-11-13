ae1a3427bf74d2df15fab740443687e19dd7a8c1
Kelsey Sry <ksry@alumni.stanford.edu>
2021-11-12 22:02:08 -0800

    404styled

---
+++ frontend/src/data/errorMessage.js
@@ -0,0 +1,22 @@
+export const errorMessage = [
+  {
+    business: "you",
+    position: {  lat:11.55831182266774, lng: 104.89415425938421}
+  },
+  {
+    business: "hit",
+    position: {  lat:11.56302083157195, lng: 104.90720052392287}
+  },
+  {
+    business: "a",
+    position: {  lat:11.563189008997085, lng:104.91998929639827}
+  },
+  {
+    business: "404",
+    position: {  lat:11.564423572724035, lng: 104.93425018774106}
+  },
+  {
+    business: "!",
+    position: {  lat:11.564801969893008, lng:104.94377739402599}
+  },
+]
