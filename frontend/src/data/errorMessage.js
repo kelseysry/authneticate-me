b7f0c98cc1c891d946c64374ecb256e080a3f793
Kelsey Sry <ksry@alumni.stanford.edu>
2022-01-16 20:47:03 -0800

    change 404 page to login or signup msg

--- frontend/src/data/errorMessage.js
+++ frontend/src/data/errorMessage.js
@@ -1,22 +1,22 @@
 export const errorMessage = [
   {
-    business: "you",
+    business: "please",
     position: {  lat:11.55831182266774, lng: 104.89415425938421}
   },
   {
-    business: "hit",
+    business: "login",
     position: {  lat:11.56302083157195, lng: 104.90720052392287}
   },
   {
-    business: "a",
+    business: "or",
     position: {  lat:11.563189008997085, lng:104.91998929639827}
   },
   {
-    business: "404",
+    business: "sign",
     position: {  lat:11.564423572724035, lng: 104.92725018774106}
   },
   {
-    business: "!",
+    business: "up!",
     position: {  lat:11.564801969893008, lng:104.94377739402599}
   },
 ]
